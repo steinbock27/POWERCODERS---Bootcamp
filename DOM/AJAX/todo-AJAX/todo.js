cdc59e813fd4993211b18f27dab3dc845602b702
Ismet Ferdi SEN <ismetferdi@gmail.com>
2021-10-21 15:27:06 +0200

    non-repetitive random element selector

---
+++ DOM/AJAX/todo-AJAX/todo.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('ol');
+
+const addBtn    = document.querySelector('button[name=add]');
+
+addBtn.addEventListener('click',addNext);
+
+
+let idNo=Array.from({length: 200}, (_, i) => i + 1);
+
+function addNext() {
+
+  idNo.sort(()=>Math.random()-0.5);
+
+  const urlAddress=`https://jsonplaceholder.typicode.com/todos/${idNo.splice(0,1)}`;
+  const li = document.createElement('li');
+
+  fetch(urlAddress)
+  .then(response => response.json())
+  .then(data => li.textContent= data.title)
+
+  container.appendChild(li);
+
+  
+
+  idNo.splice(0,1);
+  
+}
